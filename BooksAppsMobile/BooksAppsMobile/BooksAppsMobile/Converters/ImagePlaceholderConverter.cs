36909c7fe030f0378c617ee5c64bc86bc16978e4
rlarrazabal <34136363+rlarrazabal@users.noreply.github.com>
2021-01-24 13:44:41 -0600

    picture adjustment

---
+++ BooksAppsMobile/BooksAppsMobile/BooksAppsMobile/Converters/ImagePlaceholderConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BooksAppsMobile.Converters
+{
+    public class ImagePlaceholderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!string.IsNullOrWhiteSpace(value?.ToString()))
+            {
+                return ImageSource.FromUri(new Uri(value.ToString()));
+            }
+            return ImageSource.FromResource("BooksAppsMobile.Images.placeholder.jpg", typeof(ImagePlaceholderConverter).GetTypeInfo().Assembly);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
